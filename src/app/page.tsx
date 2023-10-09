23f977948ae24fbcf66ffce70781da376d6bfdfe
Jzz-C <ygyi8632@gmail.com>
2023-10-10 05:16:34 +0630

    feat: Delete unused images and test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <h1 className="text-xl font-bold text-center mt-3 text-transparent bg-clip-text bg-gradient-to-r from-red-400 via-purple-400 to-orange-700">
+        Im come back baby.
+      </h1>
+    </main>
+  );
 }
