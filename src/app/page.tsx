95594718770e17a731ad1207a5fc93845465b3bf
Jzz-C <ygyi8632@gmail.com>
2023-10-10 18:32:20 +0630

    feat: Updated custom tailwind config file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="w-full h-full flex mt-5">
       <h1 className="text-xl font-bold text-center mt-3 text-transparent bg-clip-text bg-gradient-to-r from-red-400 via-purple-400 to-orange-700">
         Im come back baby.
       </h1>
