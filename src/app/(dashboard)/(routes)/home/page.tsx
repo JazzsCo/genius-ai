0c18fb9748e1193e7795914e1186690286738cc2
Jzz-C <ygyi8632@gmail.com>
2023-10-10 18:42:09 +0630

    feat: Create route groups

---
+++ src/app/(dashboard)/(routes)/home/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="text-lg font-medium text-green-600 text-center mt-4">
+      HomePage
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/app/(dashboard)/(routes)/setting/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SettingPage() {
+  return (
+    <div className="text-lg font-medium text-green-600 text-center mt-4">
+      SettingPage
+    </div>
+  );
+}
+
+export default SettingPage;
