e463bbb17f6bfa821bacd82870990c2217f1380f
Jzz-C <ygyi8632@gmail.com>
2023-10-13 12:35:23 +0630

    Added constant

---
+++ src/constant/index.ts
@@ -0,0 +1,53 @@
+import {
+  LayoutDashboardIcon,
+  ConstructionIcon,
+  Music,
+  VideoIcon,
+  Code,
+  Settings,
+} from "lucide-react";
+
+export const routes = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+    icon: LayoutDashboardIcon,
+    color: "",
+    bgcolor: "",
+  },
+  {
+    name: "Conversation",
+    href: "/conversation",
+    icon: ConstructionIcon,
+    color: "",
+    bgcolor: "",
+  },
+  {
+    name: "Music Generation",
+    href: "/music",
+    icon: Music,
+    color: "",
+    bgcolor: "",
+  },
+  {
+    name: "Video Generation",
+    href: "/video",
+    icon: VideoIcon,
+    color: "",
+    bgcolor: "",
+  },
+  {
+    name: "Code Generation",
+    href: "/code",
+    icon: Code,
+    color: "",
+    bgcolor: "",
+  },
+  {
+    name: "Setting",
+    href: "/setting",
+    icon: Settings,
+    color: "",
+    bgcolor: "",
+  },
+];
