2f9f0db72610beb9046dbd92e2dcf02e38900405
Jzz-C <ygyi8632@gmail.com>
2023-10-18 14:16:19 +0630

    Create code api route

--- src/app/api/code/route.ts
+++ src/app/api/code/route.ts
@@ -1,1 +1,46 @@
-export async function POST(req: Request) {}
+import OpenAI from "openai";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { ChatCompletionMessage } from "openai/resources/index.mjs";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
+});
+
+const instructionMessage: ChatCompletionMessage = {
+  role: "system",
+  content: "You are code generator.",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+    const { message } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API Key is required.", {
+        status: 500,
+      });
+    }
+
+    if (!message) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, message],
+    });
+
+    return NextResponse.json(response.choices[0].message);
+  } catch (error) {
+    console.log("[CONVERSATION_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
