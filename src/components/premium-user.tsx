bb573c0394fc921b43563d895594ff5fa9d76b07
Jzz-C <ygyi8632@gmail.com>
2023-10-21 01:49:19 +0630

    Design progress ui

--- src/components/premium-user.tsx
+++ src/components/premium-user.tsx
@@ -1,5 +1,8 @@
+import { Zap } from "lucide-react";
+
 import { MAX_FREE_COUNTS } from "@/constant";
 import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 
 interface PremiumUserProps {
   userApiLimitCount?: number;
@@ -7,11 +10,20 @@ interface PremiumUserProps {
 
 const PremiumUser = ({ userApiLimitCount }: PremiumUserProps) => {
   return (
-    <div className="w-full rounded-xl p-5 text-center text-sm space-y-2 bg-white/5">
-      <p>
-        {userApiLimitCount} / {MAX_FREE_COUNTS} Free Generations
-      </p>
-      <Button className="rounded-xl w-full">Upgrade</Button>
+    <div className="w-full rounded-xl p-5 text-center text-sm space-y-4 bg-white/5">
+      <div className="space-y-1.5">
+        <p>
+          {userApiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+        </p>
+
+        <Progress
+          value={(userApiLimitCount! / MAX_FREE_COUNTS) * 100}
+          className="h-3"
+        />
+      </div>
+      <Button className="rounded-xl w-full bg-gradient-to-r from-pink-500 via-red-400 to-amber-500 text-black/90 font-semibold">
+        Upgrade <Zap className="w-4 h-4 ml-2 fill-slate-800" />
+      </Button>
     </div>
   );
 };
