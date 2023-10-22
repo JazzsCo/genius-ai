bcc42c2f7ae5953fb494beaa042ad3eaef9ede95
Jzz-C <ygyi8632@gmail.com>
2023-10-22 14:36:44 +0630

    Create stripe api route

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,14 +1,66 @@
-import { auth, currentUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+
+import prisma from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/constant";
 
 export async function GET() {
   try {
     const { userId, user } = auth();
+    const settingUrl = absoluteUrl("/setting");
 
     if (!userId || !user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const userSubscription = await prisma.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingUrl,
+      });
+
+      return NextResponse.json({ url: stripeSession.url });
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      success_url: settingUrl,
+      cancel_url: settingUrl,
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      customer_email: user.emailAddresses[0].emailAddress,
+      metadata: {
+        userId,
+      },
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Genius Ai",
+              description: "Unlimited AI Generations",
+            },
+            unit_amount: 2000,
+            recurring: {
+              interval: "month",
+            },
+          },
+        },
+      ],
+    });
+
+    return NextResponse.json({ url: stripeSession.url });
   } catch (error) {
-    console.log(error);
+    console.log("[STRIPE_ERROR]", error);
+
+    return new NextResponse("Internal server error", { status: 500 });
   }
 }
