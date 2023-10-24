4e43ed80e8bd4cbddd3ed543207098aedbe1218c
Jzz-C <ygyi8632@gmail.com>
2023-10-24 19:02:02 +0630

    Added check user subscription

---
+++ src/lib/subscription.ts
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs";
+
+import prisma from "@/lib/prismadb";
+import { DAY_IN_MS } from "@/constant";
+
+export const subscription = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return false;
+  }
+
+  const userSubscription = await prisma.userSubscription.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (!userSubscription) {
+    return false;
+  }
+
+  const isValid =
+    userSubscription.stripePriceId &&
+    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+      Date.now();
+
+  return !!isValid;
+};
